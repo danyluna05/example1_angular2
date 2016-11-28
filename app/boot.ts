96a593620febfabe8e729985c3b55399a47f021b
daniela fernanda garcia betancourt <daniela fernanda garcia betancourt>
2016-11-27 20:18:13 -0500

    first commit

---
+++ app/boot.ts
@@ -0,0 +1,5 @@
+import { bootstrap } from 'angular2/platform/browser';
+import { FormsModule }   from '@angular/forms';
+import {AppComponent} from './components/heroe/app.componet';
+
+bootstrap(AppComponent);
---
+++ built/app/boot.js
@@ -0,0 +1,5 @@
+"use strict";
+var browser_1 = require('angular2/platform/browser');
+var app_componet_1 = require('./components/heroe/app.componet');
+browser_1.bootstrap(app_componet_1.AppComponent);
+//# sourceMappingURL=boot.js.map
